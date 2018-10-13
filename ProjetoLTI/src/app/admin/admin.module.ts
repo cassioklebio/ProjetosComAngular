078b9b94ae05a852061739f26eb2c432fed6ca53
cassioklebio <cassiotr2009@gmail.com>
2018-10-12 23:05:06 -0300

    Projeto de estudo com Angular

---
+++ ProjetoLTI/src/app/admin/admin.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProdutosComponent } from './produtos/produtos.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ProdutosComponent]
+})
+export class AdminModule { }
